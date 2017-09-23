2c288fb9ba2f24d74da2a11d82b6cf0ab377228c
ktruckenmiller <kmtruckenmiller@gmail.com>
2017-09-23 10:36:57 -0500

    update

--- push_up.py
+++ push_up.py
@@ -12,13 +12,14 @@ s3 = boto3.resource('s3')
 sts = boto3.client('sts')
 
 account_num = os.environ.get('PLUGIN_AWS_ACCOUNT', sts.get_caller_identity()['Account'])
-zip_files = os.environ.get('PLUGIN_ZIP_FILES', 'deployment.zip')
+zip_files = os.environ.get('PLUGIN_ZIP_FILES', 'deployment.zip').split(',')
 region = os.environ.get('PLUGIN_REGION', 'us-west-2')
-bucket = os.environ.get('PLUGIN_BUCKET', 'lambda-deploy-{}-{}'.format(account_num, region))
+bucket = os.environ.get('PLUGIN_BUCKET', 'deploy-artifacts-{}-{}'.format(account_num, region))
 
 for f in zip_files:
     print(f)
     print(zip_files)
     print(region)
     print(bucket)
+    print(os.environ.get('PLUGIN_BOSTON'))
     # s3.meta.client.upload_file(f, bucket, "{}/{}/{}/{}".format(prefix, deployable_name, sha, os.path.basename(f)))
--- test-it.sh
+++ test-it.sh
@@ -1,10 +1,10 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 docker build -t ktruckenmiller/drone-s3 .
 # docker run --rm -it \
+#   --entrypoint="sh" \
 #   -e PLUGIN_BDP_FILE=".bdp" \
 #   -e CI_WORKSPACE=$(pwd) \
 #   -v $(pwd):/work \
-#   -v $(pwd)/code/push_up.py:/push_up.py \
 #   ktruckenmiller/drone-s3
 docker push ktruckenmiller/drone-s3
 git add .
